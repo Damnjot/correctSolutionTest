cd61b883c577a26f338cb4b7d0a4fe577963fc76
Damnjot <236475@student.pwr.edu.pl>
2019-11-17 13:15:50 +0100

    smth

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args){
+
+        Scanner myObj = new Scanner(System.in);
+
+        int size = myObj.nextInt();
+        int[] numbers = new int[size];
+        int i=0;
+        while(myObj.hasNext() && i<size){
+            numbers[i]=myObj.nextInt();
+            i++;
+        }
+        if (i == 0) {
+            System.out.println(0);
+            System.exit(0);
+        }
+
+        if (args[0].equals("--add")) {
+            add(numbers);
+        } else if (args[0].equals("--multiply")) {
+            multiply(numbers);
+        } else {
+            System.out.println("Wrong parameters.");
+            System.exit(0);
+        }
+
+    }
+
+    static void add(int[] numbers){
+        int result = IntStream.of(numbers).sum();
+        System.out.println(result);
+    }
+
+    static void multiply(int[] numbers){
+        int result = 1;
+        for (int n : numbers) {
+            result *= n;
+        }
+        System.out.println(result);
+    }
+}
